f7150f6d4001f4afb647c4128fc6a8d771cb68c4
jmyunsffsd <139943947+jmyunsffsd@users.noreply.github.com>
2023-07-21 10:53:07 +0900

    Add files via upload

---
+++ DHT11_LCD_.ino
@@ -0,0 +1,62 @@
+#include <SimpleDHT.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2);
+// for DHT11, 
+//      VCC: 5V or 3V
+//      GND: GND
+//      DATA: 2
+int pinDHT11 = 2;
+SimpleDHT11 dht11(pinDHT11);
+
+void setup() { 
+  Serial.begin(115200);
+  
+  lcd.init();
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(3,0);
+  lcd.print("Hello, world!");
+  lcd.setCursor(2,1);
+  lcd.print("Ywrobot Arduino!");
+  
+}
+
+void loop() {
+  // start working...
+  Serial.println("=================================");
+  Serial.println("Sample DHT11...");
+  
+  // read without samples.
+  byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err="); Serial.print(SimpleDHTErrCode(err));
+    Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(1000);
+    return;
+  }
+  
+  Serial.print("Sample OK: ");
+  Serial.print((int)temperature); Serial.print(" *C, "); 
+  Serial.print((int)humidity); Serial.println(" H");
+  int t=temperature;
+ int h=humidity;
+  lcd.init();
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("temper = ");
+  lcd.setCursor(7,0);
+  lcd.print(t);
+  lcd.setCursor(9,0);
+  lcd.write(223);
+  lcd.setCursor(0,1);
+  lcd.print("humi = ");
+  lcd.setCursor(7,1);
+  lcd.print(humidity);
+  lcd.setCursor(11,1);
+  lcd.print("%");
+   // DHT11 sampling rate is 1HZ.
+  delay(1500);
+}
